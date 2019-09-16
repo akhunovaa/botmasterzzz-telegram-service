6becf020d46f12e8945d7ff6b78ad8009c86fb84
Azat Akhunov <azat.akhunov@protonmail.com>
2019-09-17 00:55:55 +0300

    [СF] to statistic collection callback data was added

--- src/main/java/com.botmasterzzz.telegram/util/HelperUtil.java
+++ src/main/java/com.botmasterzzz.telegram/util/HelperUtil.java
@@ -1,6 +1,9 @@
 package com.botmasterzzz.telegram.util;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.URL;
 
 public class HelperUtil {
 
@@ -16,5 +19,14 @@ public class HelperUtil {
         }
 
         return remoteAddr;
+    }
+
+    public static String saveImage(String imageUrl, String destinationFile) throws IOException {
+        String filePath = System.getProperty("java.io.tmpdir") + "/" + destinationFile;
+        File file = new File(filePath);
+        if(!file.exists()){
+            ImageIO.write(ImageIO.read(new URL(imageUrl)), "jpg", new File(filePath));
+        }
+        return filePath;
     }
 }
