816cb5094a9cfb419623083186a3125898dba784
Azat Akhunov <azat.akhunov@protonmail.com>
2019-09-16 23:52:11 +0300

    [BF] send photo fixed

--- src/main/java/com.botmasterzzz.telegram/controller/BotApiMethodController.java
+++ src/main/java/com.botmasterzzz.telegram/controller/BotApiMethodController.java
@@ -1,15 +1,12 @@
 package com.botmasterzzz.telegram.controller;
 
-import com.botmasterzzz.bot.api.impl.methods.BotApiMethod;
+import com.botmasterzzz.bot.api.impl.methods.PartialBotApiMethod;
 import com.botmasterzzz.bot.api.impl.objects.Update;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public abstract class BotApiMethodController {
 
@@ -22,40 +19,27 @@ public abstract class BotApiMethodController {
     public BotApiMethodController(Object bean, Method method) {
         this.bean = bean;
         this.method = method;
-
-        processUpdate = typeListReturnDetect() ? this::processList : this::processSingle;
+        processUpdate = this::processSingle;
     }
 
     public abstract boolean successUpdatePredicate(Update update);
 
-    public List<BotApiMethod> process(Update update) {
+    public PartialBotApiMethod process(Update update) {
+        PartialBotApiMethod process = null;
         try {
-            return processUpdate.accept(update);
+            process = processUpdate.accept(update);
         } catch (IllegalAccessException | InvocationTargetException e) {
             logger.error("bad invoke method", e);
         }
-        return Collections.emptyList();
+        return process;
     }
 
-    public boolean typeListReturnDetect() {
-        return List.class.equals(method.getReturnType());
-    }
-
-    private List<BotApiMethod> processSingle(Update update)
-            throws InvocationTargetException, IllegalAccessException {
-        BotApiMethod botApiMethod = (BotApiMethod) method.invoke(bean, update);
-        return botApiMethod != null ? Collections.singletonList(botApiMethod) : new ArrayList<>(0);
-    }
-
-    public List<BotApiMethod> processList(Update update)
-            throws InvocationTargetException, IllegalAccessException {
-        List<BotApiMethod> botApiMethods = (List<BotApiMethod>) method.invoke(bean, update);
-        return botApiMethods != null ? botApiMethods : new ArrayList<>(0);
+    private PartialBotApiMethod processSingle(Update update) throws InvocationTargetException, IllegalAccessException {
+        PartialBotApiMethod partialBotApiMethod = (PartialBotApiMethod) method.invoke(bean, update);
+        return partialBotApiMethod;
     }
 
     private interface Process {
-
-        List<BotApiMethod> accept(Update update)
-                throws InvocationTargetException, IllegalAccessException;
+        PartialBotApiMethod accept(Update update) throws InvocationTargetException, IllegalAccessException;
     }
 }
