6e5e6cdaef3a966a931bf4ae3f45a3bd66ab9fd3
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-14 01:03:34 +0300

    [СF] to statistic collection callback data was added

--- src/main/java/com.botmasterzzz.telegram/controller/telegram/getparts/impl/GetPartsMessageServiceImpl.java
+++ src/main/java/com.botmasterzzz.telegram/controller/telegram/getparts/impl/GetPartsMessageServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -155,7 +156,8 @@ public class GetPartsMessageServiceImpl implements GetPartsMessageService {
         String path = this.path + FILE_PATH + partId;
         logger.info("Images load from file  path: {}", path);
         logger.info("Location load: {}", new File(path).getAbsolutePath());
-        logger.info("Location load: {}", new File("/").getAbsolutePath());
+        logger.info("Location load: {}", Arrays.toString(new File(path).list()));
+        logger.info("Location load: {}", Arrays.toString(new File("/").list()));
         File file = new File(path);
         String[] files = file.list();
         int fileCount = null != files ? files.length : 0;
