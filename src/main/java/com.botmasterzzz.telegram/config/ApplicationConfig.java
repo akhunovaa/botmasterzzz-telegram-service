64c15a0038779db93a98f56f1eb18141494f12e1
Azat Akhunov <azat.akhunov@protonmail.com>
2020-07-02 14:46:17 +0300

    [СF] to statistic collection callback data was added

--- src/main/java/com.botmasterzzz.telegram/config/ApplicationConfig.java
+++ src/main/java/com.botmasterzzz.telegram/config/ApplicationConfig.java
@@ -116,7 +116,7 @@ public class ApplicationConfig implements WebApplicationInitializer {
         localSessionFactoryBean.setPackagesToScan(PROPERTY_NAME_ENTITY_LOCATION);
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
-        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.show_sql", "true");
         localSessionFactoryBean.setHibernateProperties(properties);
         return localSessionFactoryBean;
     }
