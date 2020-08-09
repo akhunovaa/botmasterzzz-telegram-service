2cf50d417833c7089ac6b376072585c8ad2a7d9a
Azat Akhunov <azat.akhunov@protonmail.com>
2020-08-09 18:07:23 +0300

    [NF] mailing added

--- src/main/java/com.botmasterzzz.telegram/service/TelegramUserService.java
+++ src/main/java/com.botmasterzzz.telegram/service/TelegramUserService.java
@@ -2,8 +2,12 @@ package com.botmasterzzz.telegram.service;
 
 import com.botmasterzzz.telegram.entity.TelegramBotUserEntity;
 
+import java.util.List;
+
 public interface TelegramUserService {
 
     TelegramBotUserEntity getTelegramUser(Long telegramUserId);
+
+    List<TelegramBotUserEntity> getTelegramUserList();
 
 }
