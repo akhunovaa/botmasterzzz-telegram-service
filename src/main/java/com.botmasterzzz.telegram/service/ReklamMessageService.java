2516af3a4a64cdb961cd0e7cd3c08778baa782d3
Azat Akhunov <azat.akhunov@protonmail.com>
2020-09-16 06:49:28 +0300

    [NF] portfolio bot added

--- src/main/java/com.botmasterzzz.telegram/service/ReklamMessageService.java
+++ src/main/java/com.botmasterzzz.telegram/service/ReklamMessageService.java
@@ -1,7 +1,4 @@
 package com.botmasterzzz.telegram.service;
 
-import com.botmasterzzz.bot.api.impl.objects.replykeyboard.InlineKeyboardMarkup;
-
-
 public interface ReklamMessageService {
 }
--- src/main/java/com.botmasterzzz.telegram/service/impl/ReklamMessageServiceImpl.java
+++ src/main/java/com.botmasterzzz.telegram/service/impl/ReklamMessageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.botmasterzzz.telegram.service.impl;
 
 import com.botmasterzzz.telegram.service.ReklamMessageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReklamMessageServiceImpl implements ReklamMessageService {
 }
